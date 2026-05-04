37a8a1420e26a61927b54c8fa756bd92cc87d76f
Mirash1l1 <killer28392@gmail.com>
2026-05-04 17:59:53 +0300

    Добавлено округление в 'rate_service', все тесты проходят !

--- app/main.py
+++ app/main.py
@@ -47,9 +47,11 @@ class CarWashStation:
             single_car_object.clean_mark = self.clean_power
 
     def rate_service(self, number_rating: float) -> None:
-        self.average_rating = (
-            self.average_rating * self.count_of_ratings
-            + number_rating
-        ) / (self.count_of_ratings + 1)
-
-        self.count_of_ratings += 1
+            self.average_rating = round(
+                (
+                        self.average_rating * self.count_of_ratings
+                        + number_rating
+                ) / (self.count_of_ratings + 1),
+                1
+            )
+            self.count_of_ratings += 1
